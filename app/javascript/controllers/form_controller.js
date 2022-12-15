fb4e6801493a9b6bea3ac369234f2087e23bf889
Alexandra Leschi <alexandra.leschi@hotmail.fr>
2022-12-15 17:34:56 +0100

    ajout form debut gestion

--- app/javascript/controllers/form_controller.js
+++ app/javascript/controllers/form_controller.js
@@ -3,7 +3,8 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static get targets() {
   return ['form','submitBouton',
-  'datereception',
+  'datereception','dateenvoi','statut','aei','cpi','t2i','etpti','aef','cpf','t2f','etptf','aeresult','cpresult','t2result','etptresult','commentaire',
+  'Btnvalidate','Btnsave','count','datealerte1','datealerte2',
   ];
   }
   connect() {
@@ -14,6 +15,22 @@ export default class extends Controller {
     e.preventDefault();
     let isValid = this.validateForm(this.formTarget);
 
+  }
+
+  count(e){
+    e.preventDefault();
+  }
+
+  changeDate1(e){
+    e.preventDefault();
+    const date_max = new Date(2023, 2, 1);  
+    console.log(this.datereceptionTarget.value);
+    console.log(new Date(this.datereceptionTarget.value) < date_max);
+
+  }
+  changeDate2(e){
+    e.preventDefault();
+    const date_max = new Date(2023, 2, 15)  
   }
 
 
@@ -31,16 +48,12 @@ export default class extends Controller {
 
     return isValid
   }
-  preview(e){
+  save(e){
     e.preventDefault();
     let isValid = this.validateForm(this.formTarget);
     
   }
 
-  modifier(e){
-    e.preventDefault();
-
-  }
 
   replaceHtml(form, result){
     if (form.value == ""){
