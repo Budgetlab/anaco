41f8a947b55eb8d83376f70028df737a62c338c0
Alexandra Leschi <alexandra.leschi@hotmail.fr>
2022-12-23 12:05:28 +0100

    ajout crg2

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -11,6 +11,28 @@ class PagesController < ApplicationController
 		elsif Date.today > @date2
 			@phase = "CRG2"
 		end
+		@avis_favorables = Avi.where('phase = ? AND etat != ? AND statut = ?', "Début de gestion",'Brouillon','Favorable').count
+		@avis_reserves = Avi.where('phase = ? AND etat != ? AND statut = ?', "Début de gestion",'Brouillon',"Favorable avec réserve").count
+		@avis_defavorables = Avi.where('phase = ? AND etat != ? AND statut = ?', "Début de gestion",'Brouillon','Défavorable').count
+		@avis_vide = current_user.bops.count - @avis_favorables - @avis_reserves - @avis_defavorables
+		@avis = [@avis_favorables,@avis_reserves,@avis_defavorables,@avis_vide]
+
+		@notes1 = []
+		if @date1 < Date.today
+			@notes1_sans_risque = Avi.where('phase = ? AND etat != ? AND statut = ?', "CRG1",'Brouillon','Aucun risque').count
+			@notes1_moyen = Avi.where('phase = ? AND etat != ? AND statut = ?', "CRG1",'Brouillon',"Risques éventuels ou modérés").count
+			@notes1_risque = Avi.where('phase = ? AND etat != ? AND statut = ?', "CRG1",'Brouillon','Risques certains ou significatifs').count
+			@notes1_vide = current_user.bops.count - @notes1_sans_risque - @notes1_moyen - @notes1_risque
+			@notes1 = [@notes1_sans_risque,@notes1_moyen,@notes1_risque,@notes1_vide]
+		end
+		@notes2 = []
+		if @date2 < Date.today
+			@notes2_sans_risque = Avi.where('phase = ? AND etat != ? AND statut = ?', "CRG2",'Brouillon','Aucun risque').count
+			@notes2_moyen = Avi.where('phase = ? AND etat != ? AND statut = ?', "CRG2",'Brouillon',"Risques éventuels ou modérés").count
+			@notes2_risque = Avi.where('phase = ? AND etat != ? AND statut = ?', "CRG2",'Brouillon','Risques certains ou significatifs').count
+			@notes2_vide = current_user.bops.count - @notes2_sans_risque - @notes2_moyen - @notes2_risque
+			@notes2 = [@notes2_sans_risque,@notes2_moyen,@notes2_risque,@notes2_vide]
+		end
 	end
 
 	def error_404
---
+++ app/javascript/controllers/highcharts_controller.js
@@ -0,0 +1,269 @@
+import { Controller } from "@hotwired/stimulus"
+import Highcharts from "highcharts"
+import exporting from "exporting"
+import data from "data"
+import accessibility from "accessibility"
+import nodata from "nodata"
+exporting(Highcharts)
+data(Highcharts)
+accessibility(Highcharts)
+nodata(Highcharts)
+
+export default class extends Controller {
+  static get targets() {
+  return ['canvasAvis','canvasNotes1','canvasNotes2',
+  ];
+  }
+  connect() {
+    this.syntheseAvis();
+    this.showViz();
+  }
+
+  showViz(){
+      const notes1 = JSON.parse(this.data.get("notes1"));
+      const notes2 = JSON.parse(this.data.get("notes2"));
+      if (notes1.length > 0) {
+        this.syntheseNote1();
+      }
+      if (notes2.length > 0) {
+          this.syntheseNote2();
+      }
+  }
+    syntheseAvis(){
+    const data = JSON.parse(this.data.get("avis"));
+    const colors = ["var(--background-action-low-green-bourgeon)","var(--artwork-minor-blue-france)","var(--artwork-minor-pink-macaron)", "var(--background-contrast-pink-macaron)"]
+    const options = {
+          chart: {
+                height:'100%',
+                style:{
+                    fontFamily: "Marianne",
+                },
+                plotBackgroundColor: null,
+                plotBorderWidth: null,
+                plotShadow: false,
+                type: 'pie',  
+                                  
+          },
+          exporting:{enabled: false},
+          colors: Highcharts.map(colors, function (color) {
+              return {
+                  radialGradient: {
+                      cx: 0.5,
+                      cy: 0.3,
+                      r: 0.7
+                  },
+                  stops: [
+                      [0, color],
+                      [1, Highcharts.color(color).brighten(-0.3).get('rgb')] // darken
+                  ]
+              };
+          }),
+          
+          title: {
+              text: 'Répartition des avis',
+             
+              style: {
+                fontSize: '13px',
+                fontWeight: "900",
+                color: 'var(--text-title-grey)',
+                },
+          },
+          tooltip: {
+              borderColor: 'transparent',
+              borderRadius: 16,
+              backgroundColor: "rgba(245, 245, 245, 1)",
+              formatter: function () {
+                return '<b>' + this.point.name +': </b>' + this.point.y + ' (' + Math.round(this.percentage*10)/10 + '% )'
+              }
+              //pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+          },
+          accessibility: {
+              point: {
+                  valueSuffix: '%'
+              }
+          },
+          plotOptions: {
+              pie: {
+                  size: '100%',
+                  innerSize: '80%',
+                  allowPointSelect: true,
+                  cursor: 'pointer',
+                  dataLabels: {
+                      enabled: false
+                  },
+                  showInLegend: true,
+              }
+          },
+          series: [{
+              name: 'Catégorie',
+              data: [
+                  { name: 'Avis favorables', y: data[0] },
+                  { name: 'Avis favorables avec réserves', y: data[1] },
+                  { name: 'Avis défavorables', y: data[2] },
+                  { name: 'Avis non renseignés', y: data[3] }
+              ]
+          }]
+    }
+    this.chart = new Highcharts.chart(this.canvasAvisTarget, options);
+    this.chart.reflow();
+  }
+
+    syntheseNote1(){
+        const data = JSON.parse(this.data.get("note1"));
+        const colors = ["var(--background-action-low-green-bourgeon)","var(--artwork-minor-blue-france)","var(--artwork-minor-pink-macaron)", "var(--background-contrast-pink-macaron)"]
+        const options = {
+            chart: {
+                height:'100%',
+                style:{
+                    fontFamily: "Marianne",
+                },
+                plotBackgroundColor: null,
+                plotBorderWidth: null,
+                plotShadow: false,
+                type: 'pie',
+
+            },
+            exporting:{enabled: false},
+            colors: Highcharts.map(colors, function (color) {
+                return {
+                    radialGradient: {
+                        cx: 0.5,
+                        cy: 0.3,
+                        r: 0.7
+                    },
+                    stops: [
+                        [0, color],
+                        [1, Highcharts.color(color).brighten(-0.3).get('rgb')] // darken
+                    ]
+                };
+            }),
+
+            title: {
+                text: 'Répartition des notes',
+
+                style: {
+                    fontSize: '13px',
+                    fontWeight: "900",
+                    color: 'var(--text-title-grey)',
+                },
+            },
+            tooltip: {
+                borderColor: 'transparent',
+                borderRadius: 16,
+                backgroundColor: "rgba(245, 245, 245, 1)",
+                formatter: function () {
+                    return '<b>' + this.point.name +': </b>' + this.point.y + ' (' + Math.round(this.percentage*10)/10 + '% )'
+                }
+                //pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+            },
+            accessibility: {
+                point: {
+                    valueSuffix: '%'
+                }
+            },
+            plotOptions: {
+                pie: {
+                    size: '100%',
+                    innerSize: '80%',
+                    allowPointSelect: true,
+                    cursor: 'pointer',
+                    dataLabels: {
+                        enabled: false
+                    },
+                    showInLegend: true,
+                }
+            },
+            series: [{
+                name: 'Catégorie',
+                data: [
+                    { name: 'BOP avec capacité contributive', y: data[0] },
+                    { name: 'BOP avec consommation à la ressource', y: data[1] },
+                    { name: 'BOP avec besoin de financement', y: data[2] },
+                    { name: 'Notes non renseignés', y: data[3] }
+                ]
+            }]
+        }
+        this.chart = new Highcharts.chart(this.canvasNote1Target, options);
+        this.chart.reflow();
+    }
+
+    syntheseNote2(){
+        const data = JSON.parse(this.data.get("note2"));
+        const colors = ["var(--background-action-low-green-bourgeon)","var(--artwork-minor-blue-france)","var(--artwork-minor-pink-macaron)", "var(--background-contrast-pink-macaron)"]
+        const options = {
+            chart: {
+                height:'100%',
+                style:{
+                    fontFamily: "Marianne",
+                },
+                plotBackgroundColor: null,
+                plotBorderWidth: null,
+                plotShadow: false,
+                type: 'pie',
+
+            },
+            exporting:{enabled: false},
+            colors: Highcharts.map(colors, function (color) {
+                return {
+                    radialGradient: {
+                        cx: 0.5,
+                        cy: 0.3,
+                        r: 0.7
+                    },
+                    stops: [
+                        [0, color],
+                        [1, Highcharts.color(color).brighten(-0.3).get('rgb')] // darken
+                    ]
+                };
+            }),
+
+            title: {
+                text: 'Répartition des notes',
+
+                style: {
+                    fontSize: '13px',
+                    fontWeight: "900",
+                    color: 'var(--text-title-grey)',
+                },
+            },
+            tooltip: {
+                borderColor: 'transparent',
+                borderRadius: 16,
+                backgroundColor: "rgba(245, 245, 245, 1)",
+                formatter: function () {
+                    return '<b>' + this.point.name +': </b>' + this.point.y + ' (' + Math.round(this.percentage*10)/10 + '% )'
+                }
+                //pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+            },
+            accessibility: {
+                point: {
+                    valueSuffix: '%'
+                }
+            },
+            plotOptions: {
+                pie: {
+                    size: '100%',
+                    innerSize: '80%',
+                    allowPointSelect: true,
+                    cursor: 'pointer',
+                    dataLabels: {
+                        enabled: false
+                    },
+                    showInLegend: true,
+                }
+            },
+            series: [{
+                name: 'Catégorie',
+                data: [
+                    { name: 'BOP avec capacité contributive', y: data[0] },
+                    { name: 'BOP avec consommation à la ressource', y: data[1] },
+                    { name: 'BOP avec besoin de financement', y: data[2] },
+                    { name: 'Notes non renseignés', y: data[3] }
+                ]
+            }]
+        }
+        this.chart = new Highcharts.chart(this.canvasNote2Target, options);
+        this.chart.reflow();
+    }
+
+}
