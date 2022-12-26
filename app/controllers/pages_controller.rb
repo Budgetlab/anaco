0e87c06bf8ee38b691919f6f17801d193abe6147
Alexandra Leschi <alexandra.leschi@hotmail.fr>
2022-12-26 23:03:07 +0100

    update restitutions

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -71,6 +71,32 @@ class PagesController < ApplicationController
 		@t2_f = @avis.sum(:t2_f)
 		@etpt_f = @avis.sum(:etpt_f)
 
+		@avis_d = Avi.where(bop_id: @bops_id, phase: "Début de gestion").where("etat != ?","Brouillon")
+		@avis_favorables = @avis_d.where(statut: 'Favorable').count
+		@avis_reserves = @avis_d.where(statut:"Favorable avec réserve").count
+		@avis_defavorables = @avis_d.where(statut:'Défavorable').count
+		@avis_vide = @bops.count - @avis_favorables - @avis_reserves - @avis_defavorables
+		@avis = [@avis_favorables,@avis_reserves,@avis_defavorables,@avis_vide]
+		@avis_iscrg1 = @avis_d.where(is_crg1: true).count
+		@notes1 = []
+		if @date1 < Date.today
+			@avis_crg1 = Avi.where(bop_id: @bops_id, phase: "CRG1").where("etat != ?","Brouillon")
+			@notes1_sans_risque = @avis_crg1.where(statut: 'Aucun risque').count
+			@notes1_moyen = @avis_crg1.where(statut: "Risques éventuels ou modérés").count
+			@notes1_risque = @avis_crg1.where(statut: 'Risques certains ou significatifs').count
+			@notes1_vide = @avis_iscrg1 - @notes1_sans_risque - @notes1_moyen - @notes1_risque
+			@notes1 = [@notes1_sans_risque,@notes1_moyen,@notes1_risque,@notes1_vide]
+		end
+		@notes2 = []
+		if @date2 < Date.today
+			@avis_crg2 = Avi.where(bop_id: @bops_id, phase: "CRG2").where("etat != ?","Brouillon")
+			@notes2_sans_risque = @avis_crg2.where(statut: 'Aucun risque').count
+			@notes2_moyen = @avis_crg2.where(statut: "Risques éventuels ou modérés").count
+			@notes2_risque = @avis_crg2.where(statut:'Risques certains ou significatifs').count
+			@notes2_vide = @bops.count - @notes2_sans_risque - @notes2_moyen - @notes2_risque
+			@notes2 = [@notes2_sans_risque,@notes2_moyen,@notes2_risque,@notes2_vide]
+		end
+
 	end
 	def error_404
 	    if params[:path] && params[:path] == "500"
