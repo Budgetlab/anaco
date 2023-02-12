8cfb7342f32be70f33480009a03865c75f16e841
Alexandra Leschi <alexandra.leschi@hotmail.fr>
2023-02-12 18:26:54 +0100

    update bop model

--- app/models/bop.rb
+++ app/models/bop.rb
@@ -12,6 +12,10 @@ class Bop < ApplicationRecord
 	      	row_data = Hash[[headers, row].transpose]
 	      
 		  	if !User.where(nom: row_data['Identifiant']).first.nil?
+					if Bop.exists?(code: row_data['Code CHORUS du BOP'].to_s)
+						@bop = Bop.where(code: row_data['Code CHORUS du BOP'].to_s)
+						@bop.update(user_id: User.where(nom: row_data['Identifiant']).first.id, consultant: User.where(nom: row_data['DCB en consultation sur le programme'].to_s).first.id, ministere: row_data['MINISTERE'].to_s, nom_programme: row_data['Libellé programme'].to_s, numero_programme: row_data['N°Programme'].to_i )
+					end
 		       	Bop.where('code = ?',row_data['Code CHORUS du BOP'].to_s).first_or_create do |bop|         
 		          	bop.user_id = User.where(nom: row_data['Identifiant']).first.id
 		          	bop.consultant = User.where(nom: row_data['DCB en consultation sur le programme'].to_s).first.id
