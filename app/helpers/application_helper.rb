f51fe057f037ecf86103e2f34dec5d6ac820245f
Alexandra Leschi <alexandra.leschi@hotmail.fr>
2023-03-24 17:07:33 +0100

    modification format number

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,13 @@
 module ApplicationHelper
-	def format_number(number)
-		if !number.nil?
-		  number = number_with_delimiter("%.11g" % ("%.1f" % number ), locale: :fr)
-		end
-	end
+  def format_number(number)
+    if !number.nil?
+      number = number_with_delimiter("%.11g" % ("%.1f" % number ), locale: :fr)
+    end
+  end
 
-	def format_date(date)
-		if !date.nil?
-			date = date.strftime('%d/%m/%Y')
-		end
-	end
+  def format_date(date)
+    if !date.nil?
+      date = date.strftime('%d/%m/%Y')
+    end
+  end
 end
