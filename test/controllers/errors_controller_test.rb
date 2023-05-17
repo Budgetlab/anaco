4a14d7c8b5d32c1c66b05ac07032f21d48c5f652
alexandra <alexandra.leschi@hotmail.fr>
2023-05-17 16:58:15 +0200

    modification pages test unitaires erreurs

--- test/controllers/errors_controller_test.rb
+++ test/controllers/errors_controller_test.rb
@@ -2,17 +2,18 @@ require "test_helper"
 
 class ErrorsControllerTest < ActionDispatch::IntegrationTest
   test "should get error_500" do
-    get errors_error_500_url
-    assert_response :success
+    get '/500'
+    assert_response :error
+    assert response.status == 500
   end
 
   test "should get error_404" do
-    get errors_error_404_url
-    assert_response :success
+    get '/404'
+    assert_response :not_found
+    assert response.status == 404
   end
-
   test "should get error_503" do
-    get errors_error_503_url
-    assert_response :success
+    get '/503'
+    assert response.status == 503
   end
 end
