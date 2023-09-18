a933ab973f11cd138de1786926f81f9ac39b5845
alexandra <alexandra.leschi@hotmail.fr>
2023-09-18 09:33:39 +0200

    suppression debut et fin 2023 dans form + redirection page erreurs

--- config/routes.rb
+++ config/routes.rb
@@ -39,6 +39,7 @@ Rails.application.routes.draw do
   match '/500', via: :all, to: 'errors#error_500'
   match '/404', via: :all, to: 'errors#error_404'
   match '/503', via: :all, to: 'errors#error_503'
+  match '*path', to: 'errors#error_404', via: :all
 end
   get '/', to: redirect('/anaco')
   #get '/500', to: redirect('/anaco/500')
