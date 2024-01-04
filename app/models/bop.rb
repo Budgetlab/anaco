dd1ffad7a3cd98b643fe7a418082f2e4ea78c7f4
alexandra <alexandra.leschi@hotmail.fr>
2024-01-04 08:55:37 +0100

    modification importation fichier bop et form credits

--- app/models/bop.rb
+++ app/models/bop.rb
@@ -9,13 +9,11 @@ class Bop < ApplicationRecord
     data.each_with_index do |row, idx|
       next if idx == 0 # skip header
       row_data = Hash[[headers, row].transpose]
-      @bop = Bop.where(code: row_data['Code CHORUS du BOP'].to_s)
-      @bop.update(dotation: row_data['dotation']) if @bop && !row_data['dotation'].nil?
       unless User.where(nom: row_data['Identifiant']).first.nil?
         if Bop.exists?(code: row_data['Code CHORUS du BOP'].to_s)
-          @bop = Bop.where(code: row_data['Code CHORUS du BOP'].to_s)
+          @bop = Bop.where(code: row_data['Code CHORUS du BOP'].to_s).first
           @bop.update(user_id: User.where(nom: row_data['Identifiant']).first.id, consultant: User.where(nom: row_data['DCB en consultation sur le programme'].to_s).first.id, ministere: row_data['MINISTERE'].to_s, nom_programme: row_data['Libellé programme'].to_s, numero_programme: row_data['N°Programme'].to_i )
-				else
+        else
           bop = Bop.new
           bop.user_id = User.where(nom: row_data['Identifiant']).first.id
           bop.consultant = User.where(nom: row_data['DCB en consultation sur le programme'].to_s).first.id
