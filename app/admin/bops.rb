8c5289173cad74300712761c6fa5e4ad9b7e4776
alexandra <alexandra.leschi@hotmail.fr>
2024-07-18 10:11:37 +0200

    - ajout active admin

---
+++ app/admin/bops.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register Bop do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :user_id, :ministere, :numero_programme, :nom_programme, :code, :dotation, :consultant_id, :programme_id
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:user_id, :ministere, :numero_programme, :nom_programme, :code, :dotation, :consultant_id, :programme_id]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
