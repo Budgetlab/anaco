5a884e7d176479d13cd617244cfc18889fddd51a
alexandra <alexandra.leschi@hotmail.fr>
2025-03-12 16:09:48 +0100

    update format number

--- app/javascript/controllers/acte_form_controller.js
+++ app/javascript/controllers/acte_form_controller.js
@@ -1,14 +1,17 @@
-import { Controller } from "@hotwired/stimulus"
+import {Controller} from "@hotwired/stimulus"
 
 // Connects to data-controller="form-submit"
 export default class extends Controller {
-    static targets = ["submitButton", "fieldRequire","submitAction"]
+    static targets = ["submitButton", "fieldRequire", "submitAction", "form"]
+
     connect() {
-        if (this.submitButtonTarget.dataset.conditionsMet != undefined){
+        if (this.submitButtonTarget.dataset.conditionsMet != undefined) {
             this.checkValidation()
-        } else if (this.hasSubmitButtonTarget){
+        } else if (this.hasSubmitButtonTarget) {
             this.checkSubmission();
         }
+
+        this.setNombreInput();
     }
 
     // pour check soumission à la validation
@@ -20,7 +23,7 @@ export default class extends Controller {
     }
 
     // pour check validation finale
-    checkSubmission(){
+    checkSubmission() {
         const allFieldsFilled = this.fieldRequireTargets.every(field => field.value.trim() !== "")
         this.submitButtonTarget.disabled = !allFieldsFilled
     }
@@ -32,11 +35,83 @@ export default class extends Controller {
     keepCurrentState(event) {
         this.submitActionTarget.value = "en cours d'instruction"
     }
+
     setInstruction(event) {
         this.submitActionTarget.value = "en cours d'instruction"
     }
 
-    confirmValidation(event){
+    confirmValidation(event) {
         this.submitActionTarget.value = "clôturé"
+    }
+
+    setNombreInput() {
+        const fields = document.querySelectorAll("[data-acte-form-number-field]");
+        fields.forEach(field => {
+            this.changeFloatToText(field);
+        });
+    }
+
+    changeFloatToText(field) {
+        const parsedValue = this.numberFormat(field.value);
+        if (!isNaN(parsedValue)) {
+            // Formatage du nombre avec séparateur de milliers
+            const formattedValue = parsedValue.toLocaleString("fr-FR");
+            field.value = formattedValue;
+
+        } else {
+            field.value = null;
+        }
+    }
+
+    numberFormat(number) {
+        if (number != undefined) {
+            const sanitizedValue = number.replace(/\u202F/g, "");
+            // Remplacez la virgule par un point pour permettre les décimaux
+            const sanitizedValueWithDot = sanitizedValue.replace(',', '.');
+            // Analysez la valeur en tant que nombre à virgule flottante
+            const parsedValue = parseFloat(sanitizedValueWithDot);
+            return parsedValue;
+        }
+    }
+
+    changeTextToFloat(event) {
+        event.preventDefault();
+        const fields = document.querySelectorAll("[data-acte-form-number-field]");
+        fields.forEach(field => {
+            const parsedValue = this.numberFormat(field.value);
+            if (!isNaN(parsedValue)) {
+                field.value = parsedValue;
+            }
+        })
+        this.formTarget.submit();
+    }
+
+    changeNumber(event) {
+        const inputElement = event.target;
+        const orginalLength = inputElement.value.length
+        const end = inputElement.selectionEnd;
+        let element = inputElement.value.replace(/[^0-9,-.]/g, "");
+        element = element.replace(/,,/g, ',')
+        const lastLetter = inputElement.value[inputElement.value.length - 1];
+        if (inputElement.value.length == 1 && inputElement.value == "-") {
+            inputElement.value = "-";
+        } else {
+            const parsedValue = this.numberFormat(element);
+            if (!isNaN(parsedValue)) {
+                // Formatage du nombre avec séparateur de milliers
+                const formattedValue = parsedValue.toLocaleString("fr-FR");
+                // Mettez à jour la valeur du champ de formulaire avec le format souhaité
+                if (lastLetter == "," || lastLetter == ".") {
+                    inputElement.value = formattedValue + ",";
+                } else {
+                    inputElement.value = formattedValue;
+                }
+                const lengthDiff = inputElement.value.length - orginalLength;
+                inputElement.setSelectionRange(end + lengthDiff, end + lengthDiff);
+            } else {
+                inputElement.value = null;
+            }
+        }
+
     }
 }
