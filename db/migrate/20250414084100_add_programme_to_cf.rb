d12158fb1aeeb1c9d09abded24b07de32c130281
alexandra <alexandra.leschi@hotmail.fr>
2025-04-22 13:21:29 +0200

    - udpate ruby version & fix accordeon

--- db/migrate/20250414084100_add_programme_to_cf.rb
+++ db/migrate/20250414084100_add_programme_to_cf.rb
@@ -1,11 +1,6 @@
 class AddProgrammeToCf < ActiveRecord::Migration[7.2]
   def self.up
     add_reference :centre_financiers, :programme,null: true, foreign_key: true
-    CentreFinancier.find_each do |cf|
-      numero_programme = cf.code[1..3]
-      programme = Programme.find_by(numero: numero_programme)
-      cf.update!(programme_id: programme.id) if programme
-    end
   end
 
   def self.down
