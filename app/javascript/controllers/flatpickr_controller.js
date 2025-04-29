d47a7888436e0247e27ca1feb4e3549ca06076b2
alexandra <alexandra.leschi@hotmail.fr>
2025-04-29 15:32:55 +0200

    - fix date modal flatpickr

--- app/javascript/controllers/flatpickr_controller.js
+++ app/javascript/controllers/flatpickr_controller.js
@@ -6,14 +6,11 @@ export default class extends Controller {
   connect() {
     if (this.fp) this.fp.destroy()
 
-    // Trouve un conteneur parent du champ, typiquement la modal
-    const modalContainer = this.element.closest('.fr-modal') || document.body
-
     this.fp = flatpickr(this.element, {
       locale: 'fr',
       dateFormat: "d/m/Y",
       defaultDate: this.element.value || null,
-      appendTo: modalContainer // 👈 très important ici
+
     })
   }
 
