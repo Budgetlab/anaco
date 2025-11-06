40cb75034a3c0ec7785447f239ed265651f3237d
alexandra <alexandra.leschi@hotmail.fr>
2025-11-06 16:28:50 +0100

    maj actes ht2

--- app/models/suspension.rb
+++ app/models/suspension.rb
@@ -4,8 +4,6 @@ class Suspension < ApplicationRecord
   validates :date_suspension, presence: true
   validates :motif, presence: true
 
-  #validate :unicite_suspension_ouverte, on: :create
-
   def self.ransackable_attributes(auth_object = nil)
     ["created_at", "date_reprise", "date_suspension", "ht2_acte_id", "id", "id_value", "motif", "observations", "updated_at"]
   end
@@ -13,9 +11,4 @@ class Suspension < ApplicationRecord
     ["ht2_acte"]
   end
 
-  private
-
-  def unicite_suspension_ouverte
-    #todo update
-  end
 end
