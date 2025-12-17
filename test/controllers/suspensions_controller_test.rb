25bdeba984ab37db824df056a43e6fad6f78280e
alexandra <alexandra.leschi@hotmail.fr>
2025-12-17 15:57:53 +0100

    suppression des tests inutilisés dans SuspensionsControllerTest

--- test/controllers/suspensions_controller_test.rb
+++ test/controllers/suspensions_controller_test.rb
@@ -1,18 +1,4 @@
 require "test_helper"
 
 class SuspensionsControllerTest < ActionDispatch::IntegrationTest
-  test "should get edit" do
-    get suspensions_edit_url
-    assert_response :success
-  end
-
-  test "should get update" do
-    get suspensions_update_url
-    assert_response :success
-  end
-
-  test "should get destroy" do
-    get suspensions_destroy_url
-    assert_response :success
-  end
 end
